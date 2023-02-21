393fe2a99a128216bc63efa3d932d1e9e1dce191
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-02-21 08:37:45 +0530

    2nd Commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,38 @@
-import React from 'react'
+import React, {useState }  from 'react'
 import logo from '../assets/pizzaLogo.png';
 import { Link } from "react-router-dom";
+import ReorderIcon from '@mui/icons-material/Reorder';
 import '../styles/Navbar.css';
+import { Button, ToggleButton } from '@mui/material';
 
 function Navbar() {
+
+  const [openLinks, setOpenLinks] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks)
+
+  };
+
   return (
     <div className='navbar'>
-        <div className='leftSide'>
+        <div className='leftSide' id={openLinks ? "open" : "close"} >
             <img src= {logo} />
+            <div className='hiddenLinks'>
+            <Link to='/' > Home </Link>
+            <Link to='/menu' > Menu </Link>
+            <Link to='/about' > About </Link>
+            <Link to='/contact' > Contact </Link>
+            </div>
         </div>
         <div className='rightSide'>
             <Link to='/' > Home </Link>
             <Link to='/menu' > Menu </Link>
             <Link to='/about' > About </Link>
             <Link to='/contact' > Contact </Link>
+            <Button onClick={toggleNavbar} >
+            <ReorderIcon /> 
+            </Button>
 
         </div>
     </div>
